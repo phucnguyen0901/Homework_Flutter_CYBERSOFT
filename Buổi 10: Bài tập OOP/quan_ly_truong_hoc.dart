9f1db03274c83c953106e75951c0d6ad0fa84fe9
Phuc Nguyen <phucnguyen.songcau@gmail.com>
2026-09-03 14:10:49 +0700

    ..

--- Buổi 10: Bài tập OOP/quan_ly_truong_hoc.dart
+++ Buổi 10: Bài tập OOP/quan_ly_truong_hoc.dart
@@ -1,156 +1,57 @@
-import 'dart:io';
-
 void main() {
-  List<Student> students = [];
-  List<Teacher> teachers = [];
-  List<Classroom> classrooms = [];
-
-  int newStudentID = 1;
-  int newTeacherID = 1;
-  int newClassroomID = 1;
-
-  String feature = '''
-  CHON SO TUONG UNG TINH NANG BEN DUOI:
-  - Quan ly danh sach hoc sinh:
-    1. Them hoc sinh
-    2. Xoa hoc sinh
-    3. Xem danh sach hoc sinh
-  - Quan ly danh sach giao vien:
-    4. Them giao vien
-    5. Xoa giao vien
-    6. Xem danh sach giao vien
-  - Quan ly lop hoc:
-    7. Mo lop moi, them giao vien va hoc sinh vao lop
-    8. Bao cao danh sach lop
-  ''';
-
-  while (true) {
-    print('*' * 10 + '\n' + feature);
-    stdout.write('--> Chon so: ');
-    String? chosenFeature = stdin.readLineSync()?.trim();
-
-    switch (chosenFeature) {
-      case '1':
-        addStudent(newStudentID++, students);
-        break;
-      case '2':
-        delete(students);
-        break;
-      case '3':
-        print('DANH SACH HOC SINH');
-        for (var student in students) {
-          student.showStudentInfo();
-        }
-        break;
-      case '4':
-        addTeacher(newTeacherID++, teachers);
-        break;
-      case '5':
-        delete(teachers);
-        break;
-      case '6':
-        print('DANH SACH GIAO VIEN');
-        for (var teacher in teachers) {
-          teacher.showTeacherInfo();
-        }
-        break;
-      case '7':
-        stdout.write('Nhap ten lop: ');
-        String name = stdin.readLineSync()?.trim() ?? '';
-        Classroom classroom = Classroom(newClassroomID++, name, [], null);
-        classroom.addTeacherIntoClass(teachers);
-        classroom.addStudentIntoClass(students);
-        classrooms.add(classroom);
-        break;
-      case '8':
-        print('BAO CAO LOP');
-        for (var classroom in classrooms) {
-          classroom.showClassroomInfo();
-        }
-        break;
-      default:
-        print('Khong hop le.');
-        break;
-    }
-
-    stdout.write('\nNhap "y" de tiep tuc, nhap bat ky de thoat: ');
-    String? continueInput = stdin.readLineSync()?.trim();
-    if (continueInput?.toLowerCase() != 'y') {
-      print('Da thoat ct');
-      break;
-    }
+  Student std1 = Student(
+    id: 1,
+    name: 'Nguyễn A',
+    age: 18,
+    gender: 'Nam',
+    grade: 'flutter',
+    score: [],
+  );
+  Student std2 = Student(
+    id: 2,
+    name: 'Trần A',
+    age: 18,
+    gender: 'Nam',
+    grade: 'flutter',
+    score: [8, 8, 8, 7],
+  );
+  Student std3 = Student(
+    id: 3,
+    name: 'Lê C',
+    age: 18,
+    gender: 'Nữ',
+    grade: 'flutter',
+    score: [8, 8, 8, 7, 9],
+  );
+  List<Student> listStudent = [std1, std2, std3];
+  print('====== THÔNG TIN HỌC SINH ======');
+  for (var student in listStudent) {
+    student.showStudentInfo();
   }
-}
+  print('\n');
 
-//làm liều lần 1: quyết định thử record để tái sử dụng...
-(String name, int age, String gender) inputBaseInfo() {
-  stdout.write(' - Ten: ');
-  String name = stdin.readLineSync()?.trim() ?? '';
-  if (name.isEmpty) print('Khong duoc de trong');
-
-  stdout.write(' - Tuoi: ');
-  int age = int.tryParse(stdin.readLineSync()?.trim() ?? '') ?? 0;
-  if (age <= 0) print('Tuoi khong hop le');
-
-  // làm liều lần 2: chưa nghĩ ra cách xử lí phù hợp hơn. hihi
-  stdout.write(' - Gioi tinh (0 = Nu , 1 = Nam): ');
-  int genderInput = int.tryParse(stdin.readLineSync()?.trim() ?? '') ?? 2;
-  if (genderInput != 0 && genderInput != 1) print('Nhap so khong phu hop');
-  String gender = (genderInput == 0) ? 'Nu' : 'Nam';
-
-  return (name, age, gender);
-}
-
-void addStudent(int id, List<Student> students) {
-  print('Nhap thong tin hoc sinh');
-  var (name, age, gender) = inputBaseInfo();
-  stdout.write(' - Lớp: ');
-  String grade = stdin.readLineSync()?.trim() ?? '';
-  stdout.write(' - Điểm: ');
-  double score = double.tryParse(stdin.readLineSync()?.trim() ?? '') ?? 0;
-
-  Student student = Student(id, name, age, gender, grade, score);
-  students.add(student);
-  print('Them hoc sinh thanh coong');
-}
-
-void addTeacher(int id, List<Teacher> teachers) {
-  print('Nhap thong tin giao vien');
-  var (name, age, gender) = inputBaseInfo();
-  stdout.write(' - Mon giang day: ');
-  String subject = stdin.readLineSync()?.trim() ?? '';
-  stdout.write(' - Muc luong: ');
-  int salary = int.tryParse(stdin.readLineSync()?.trim() ?? '') ?? 0;
-
-  Teacher teacher = Teacher(id, name, age, gender, subject, salary);
-  teachers.add(teacher);
-  print('Them giao vien thanh cong');
-}
-
-/*oh wow bất ngờ chưa! 
-00:30 1/9, ngồi trong nhà WC, viết 'listItem.id' bị báo lỗi gạch đỏ không nhận, 
-em chợt nghĩ "nếu các thứ cần xóa đều giống nhau ở điểm có sự hiện diện của Person 
-thì tại sao không thử cho cái này kế thừa theo luôn để lấy thẳng cái id?", 
-bèn lên mạng tìm cách kế thừa và làm theo :))
-=> thành phẩm từ "tai nạn ngọt ngào", khỏi mất công viết lại 2 cái y chang 
-cho students và teachers... hihi, dù chưa hiểu dc tại sao lại có thể làm thế...
-*/
-void delete<T extends Person>(List<T> list) {
-  if (list.isEmpty) {
-    print('Danh sach trong');
-    return;
+  Teacher teacher1 = Teacher(
+    id: 1,
+    name: 'Lý D',
+    age: 28,
+    gender: 'Nam',
+    subject: 'Flutter',
+    salary: 100_000_000,
+  );
+  List<Teacher> teachers = [teacher1];
+  print('===== THÔNG TIN GIÁO VIÊN =====');
+  for (var teacher in teachers) {
+    teacher.showTeacherInfo();
   }
-  stdout.write('Nhap ID can xoa: ');
-  int id = int.tryParse(stdin.readLineSync()?.trim() ?? '') ?? -1;
+  print('\n');
 
-  int lengthBefore = list.length;
-  list.removeWhere((listItem) => listItem.id == id);
-
-  if (list.length < lengthBefore) {
-    print('Xoa thanh cong!');
-  } else {
-    print('Khong tim thay ID nay');
+  Classroom classroom = Classroom(id: 1, name: 'Flutter');
+  // classroom.addTeacherIntoClass(teacher);
+  for (var student in listStudent) {
+    classroom.addStudentIntoClass(student);
   }
+  print('===== BÁO CÁO LỚP =====');
+  classroom.showClassroomInfo();
 }
 
 class Person {
@@ -159,30 +60,44 @@ class Person {
   int age;
   String gender;
 
-  Person(this.id, this.name, this.age, this.gender);
+  Person({
+    required this.id,
+    required this.name,
+    required this.age,
+    required this.gender,
+  });
 }
 
 class Student extends Person {
   String grade;
-  double score;
+  List<double> score;
 
-  Student(
-    super.id,
-    super.name,
-    super.age,
-    super.gender,
-    this.grade,
-    this.score,
-  );
+  Student({
+    required super.id,
+    required super.name,
+    required super.age,
+    required super.gender,
+    required this.grade,
+    required this.score,
+  });
+
+  double avgScore() {
+    if (score.isEmpty == true) return 0; //tránh lỗi chia 0 ra NaN
+
+    double total = 0;
+    for (var i in score) {
+      total += i;
+    }
+    return total / score.length;
+  }
 
   void showStudentInfo() {
-    print('  ID hoc sinh: $id');
-    print('  Ten hoc sinh: $name');
-    print('  Tuoi: $age');
-    print('  Gioi tinh: $gender');
-    print('  Lop: $grade');
-    print('  Diem: $score');
-    print('-' * 10);
+    print('  ID học sinh: $id');
+    print('  Tên học sinh: $name');
+    print('  Tuổi: $age');
+    print('  Giới tính: $gender');
+    print('  Lớp: $grade');
+    print('  Danh sách điểm: $score \n');
   }
 }
 
@@ -190,72 +105,59 @@ class Teacher extends Person {
   String subject;
   int salary;
 
-  Teacher(
-    super.id,
-    super.name,
-    super.age,
-    super.gender,
-    this.subject,
-    this.salary,
-  );
+  Teacher({
+    required super.id,
+    required super.name,
+    required super.age,
+    required super.gender,
+    required this.subject,
+    required this.salary,
+  });
 
   void showTeacherInfo() {
-    print('  ID giao vien: $id');
-    print('  Ten giao vien: $name');
-    print('  Tuoi: $age');
-    print('  Gioi tinh: $gender');
-    print('  Mon giang day: $subject');
-    print('  Muc luong: $salary');
-    print('-' * 10);
+    print('  ID giáo viên: $id');
+    print('  Tên giáo viên: $name');
+    print('  Tuổi: $age');
+    print('  Giới tính: $gender');
+    print('  Môn giảng dạy: $subject');
+    print('  Mức lương: $salary VND \n');
   }
 }
 
 class Classroom {
   int id;
   String name;
-  List<Student> students;
+  List<Student> students = [];
   Teacher? teacher;
 
-  Classroom(this.id, this.name, this.students, this.teacher);
+  Classroom({required this.id, required this.name});
 
-  void addTeacherIntoClass(List<Teacher> teachers) {
-    stdout.write('Nhap ID giao vien: ');
-    int teacherID = int.tryParse(stdin.readLineSync()?.trim() ?? '') ?? 0;
-
-    for (var findTeacher in teachers) {
-      if (findTeacher.id == teacherID) {
-        this.teacher = findTeacher;
-        print('Them giao vien phu trach thanh cong');
-        return;
-      }
-    }
-    print('Khong tim thay ID giao vien nay');
+  void addTeacherIntoClass(Teacher teacher) {
+    this.teacher = teacher;
   }
 
-  void addStudentIntoClass(List<Student> students) {
-    stdout.write('Nhap ID hoc dinh can them: ');
-    int studentID = int.tryParse(stdin.readLineSync()?.trim() ?? '') ?? 0;
-
-    for (var student in students) {
-      if (student.id == studentID) {
-        this.students.add(student);
-        print('Them hoc sinh vao lop thanh cong');
-        return;
-      }
-    }
-    print('Khong tim thay ID hoc sinh nay');
+  void addStudentIntoClass(Student student) {
+    students.add(student);
   }
 
   void showClassroomInfo() {
-    String teacherName = teacher?.name ?? 'Chua co giao vien phu trach';
-    print('ID lop hoc: $id');
-    print('Ten lop: $name');
-    print('Giao vien phu trach: $teacherName');
-    print('Danh sach hoc sinh trong lop:');
-    for (var student in students) {
-      print('- Ten hoc sinh: ${student.name}');
-      print('  Diem TB: ${student.score}');
+    print('ID lớp học: $id');
+    print('Tên lớp: $name');
+
+    String teacherName = (teacher?.name) ?? 'Chưa có giáo viên phụ trách';
+    print('Giáo viên phụ trách: $teacherName');
+
+    if (students.isEmpty == true) {
+      print('Lớp trống');
+      return;
     }
+    print('Danh sách học sinh trong lớp:');
+    for (var student in students) {
+      double avgScore = student.avgScore();
+      print('- Tên học sinh: ${student.name}');
+      print('  Điểm TB: ${avgScore.toStringAsFixed(1)}');
+    }
+
     print('-' * 10);
   }
 }
